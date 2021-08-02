67fc824e761dd7ded2530b08d8e6eb3aa3c30b86
kboul <kos.voulgaridis@gmail.com>
2021-08-02 22:15:21 +0300

    Display population per continent

--- src/features/Widgets/Widgets.tsx
+++ src/features/Widgets/Widgets.tsx
@@ -1,16 +1,15 @@
 import { Fragment } from 'react';
 
 import { Alert, Input, LineBreaker } from '../../components';
-import { getSelectedContinentsNumber, sortObjArrayByProperty } from './utils';
+import Loader from './Loader';
+import {
+  arrObjsMaxAttrValue,
+  getContinentPopulation,
+  getSelectedContinentsNumber
+} from './utils';
 import { useApi } from '../../hooks';
 import WidgetsProps from './model';
-import {
-  barTooltip,
-  baseUrl,
-  countriesAlertMsg,
-  countriesEndpoint,
-  countriesLoader
-} from './constants';
+import * as consts from './constants';
 import styles from './styles.module.sass';
 
 export default function Widgets({
@@ -18,11 +17,16 @@ export default function Widgets({
   setValue
 }: WidgetsProps) {
   const {
-    data,
+    data: countries,
     loading: countriesLoading,
     error: countriesError
-  } = useApi(`${baseUrl}${countriesEndpoint}`);
-  const countries = sortObjArrayByProperty(data, 'continent');
+  } = useApi(consts.countriesSqlOperation);
+
+  const {
+    data: population,
+    loading: populationLoading,
+    error: populationError
+  } = useApi(consts.populationSqlOperation);
 
   // eslint-disable-next-line consistent-return
   const handleBarChange = (continent: string) => () => {
@@ -37,8 +41,13 @@ export default function Widgets({
     setValue('selectedContinents', newSelectedContinents);
   };
 
-  if (countriesLoading)
-    return <div className={styles.countriesLoader}>{countriesLoader}</div>;
+  if (countriesLoading || populationLoading)
+    return (
+      <>
+        <Loader>{consts.countriesLoaderMsg}</Loader>
+        <Loader>{consts.populationLoaderMsg}</Loader>
+      </>
+    );
 
   if (countries.length > 0)
     return (
@@ -62,15 +71,23 @@ export default function Widgets({
               <Input
                 className={styles.barInput}
                 onChange={handleBarChange(continent)}
-                max={54}
+                max={arrObjsMaxAttrValue(countries, 'count')}
                 name={continent}
                 style={{
                   opacity: selectedContinents.includes(continent) ? 0.5 : 1
                 }}
-                title={barTooltip}
+                title={consts.barTooltip}
                 type="range"
                 value={count}
               />
+
+              <div
+                className={
+                  styles.population
+                }>{`Population: ${getContinentPopulation(
+                population,
+                continent
+              )}`}</div>
 
               <LineBreaker />
             </Fragment>
@@ -80,7 +97,8 @@ export default function Widgets({
       </>
     );
 
-  if (countriesError) return <Alert>{countriesAlertMsg}</Alert>;
+  if (countriesError) return <Alert>{consts.countriesAlertMsg}</Alert>;
+  if (populationError) return <Alert>{consts.populationAlertMsg}</Alert>;
 
   return null;
 }
